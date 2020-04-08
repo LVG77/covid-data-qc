1ad3d7fdb0533413409e26b87dc805eb889261df
LVG77 <lyubomir.georgiev@gmail.com>
2020-04-08 14:31:40 -0400

    Change build.R file to update the pins only if there is a data with a a new date stamp.

--- ci/build.R
+++ ci/build.R
@@ -3,6 +3,8 @@ library(rvest)
 
 pins::board_register_github(repo = "LVG77/covid-data-qc", branch = "datasets")
 
+covid_qc <- pins::pin_get("covid_qc", board = "github")
+
 url <- "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/"
 
 qc_tables <- xml2::read_html(url) %>% 
@@ -18,14 +20,12 @@ t_stamp <- time_stp %>%
   gsub("^\\s", "", .) %>% 
   as.Date("%B %d")
 
-
-cases <- extract_tbl(qc_tables, 1, "conf_cases")
-deaths <- extract_tbl(qc_tables, 3, "deaths")
-
-
-
-# write.table(cases, "data/cases_test.csv", sep = ",", col.names = !file.exists("data/cases_test.csv"), append = T, row.names = F)
-# write.table(cases, "data/deaths_test.csv", sep = ",", col.names = !file.exists("data/deaths_test.csv"), append = T, row.names = F)
-
-# write.table(cases, "data/cases.csv", sep = ",", col.names = !file.exists("data/cases.csv"), append = T, row.names = F)
-# write.table(cases, "data/deaths.csv", sep = ",", col.names = !file.exists("data/deaths.csv"), append = T, row.names = F)
+if (!t_stamp %in% covid_qc$date) {
+  cases <- extract_tbl(qc_tables, 1, "conf_cases")
+  deaths <- extract_tbl(qc_tables, 3, "deaths")
+  
+  covid_upd <- cases %>% 
+    rbind(deaths)
+  
+  pins::pin(covid_upd, name = "covid_qc", board = "github")
+}
