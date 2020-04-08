be67210806a82cda24e94e1ca24d275e47cd14db
LVG77 <lyubomir.georgiev@gmail.com>
2020-04-08 15:23:29 -0400

    Adjust to append new data and to include the data we lost from April 7th.

--- ci/build.R
+++ ci/build.R
@@ -3,7 +3,7 @@ library(rvest)
 
 pins::board_register_github(repo = "LVG77/covid-data-qc", branch = "datasets")
 
-covid_qc <- pins::pin_get("covid_qc", board = "github")
+covid_qc <- pins::pin_get("covid_qc", board = "github", cache = FALSE)
 
 url <- "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/"
 
@@ -24,8 +24,23 @@ if (!t_stamp %in% covid_qc$date) {
   cases <- extract_tbl(qc_tables, 1, "conf_cases")
   deaths <- extract_tbl(qc_tables, 3, "deaths")
   
-  covid_upd <- cases %>% 
-    rbind(deaths)
+  covid_upd <- 
+    covid_qc %>% 
+    rbind(cases) %>% 
+    rbind(deaths) 
   
   pins::pin(covid_upd, name = "covid_qc", board = "github")
 }
+
+
+
+# cases <- readr::read_csv("/Users/lvg/Desktop/cases.csv")
+# deaths <- readr::read_csv("/Users/lvg/Desktop/deaths.csv")
+# 
+# cases_df <- cases %>% 
+#   dplyr::rename(value = conf_cases) %>% 
+#   dplyr::mutate(type = "conf_cases") %>% 
+#   dplyr::select(regions, value, type, date)
+# 
+# pins::
+# pins::pin(cases_df, name = "covid_qc", board = "github")
