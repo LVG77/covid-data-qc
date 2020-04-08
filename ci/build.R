7061b280eed3186f72816d965db2254fb3e51997
LVG77 <lyubomir.georgiev@gmail.com>
2020-04-08 00:26:26 -0400

    Initial commit for build.yaml and build.R scripts that will be run by github actions.

---
+++ ci/build.R
@@ -0,0 +1,22 @@
+sapply(dir("R", full.names = T), source)
+
+url <- "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/"
+
+qc_tables <- xml2::read_html(url) %>% 
+  rvest::html_table()
+
+time_stp <- qc_tables %>% 
+  .[[1]] %>% 
+  colnames() %>% 
+  .[[2]]
+
+t_stamp <- time_stp %>% 
+  gsub(".*on|,.*", "", .) %>% 
+  gsub("^\\s", "", .) %>% 
+  as.Date("%B %d")
+
+
+cases <- extract_tbl(qc_tables, 1, "conf_cases")
+extract_tbl(qc_tables, 3, "deaths")
+
+write.table(cases, "data/cases.csv", sep = ",", col.names = !file.exists("data/cases.csv"), append = T)
