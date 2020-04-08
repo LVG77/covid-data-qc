02535e95ffe7cfbe917587cac59b1e5e691de46e
LVG77 <lyubomir.georgiev@gmail.com>
2020-04-08 11:42:46 -0400

    Change the build.R file and delete old data folder b/c I am switching to pins.

--- ci/build.R
+++ ci/build.R
@@ -1,6 +1,8 @@
 sapply(dir("R", full.names = T), source)
 library(rvest)
 
+pins::board_register_github(repo = "LVG77/covid-data-qc", branch = "datasets")
+
 url <- "https://www.quebec.ca/en/health/health-issues/a-z/2019-coronavirus/situation-coronavirus-in-quebec/"
 
 qc_tables <- xml2::read_html(url) %>% 
@@ -20,8 +22,10 @@ t_stamp <- time_stp %>%
 cases <- extract_tbl(qc_tables, 1, "conf_cases")
 deaths <- extract_tbl(qc_tables, 3, "deaths")
 
-write.table(cases, "data/cases_test.csv", sep = ",", col.names = !file.exists("data/cases_test.csv"), append = T, row.names = F)
-write.table(cases, "data/deaths_test.csv", sep = ",", col.names = !file.exists("data/deaths_test.csv"), append = T, row.names = F)
+
+
+# write.table(cases, "data/cases_test.csv", sep = ",", col.names = !file.exists("data/cases_test.csv"), append = T, row.names = F)
+# write.table(cases, "data/deaths_test.csv", sep = ",", col.names = !file.exists("data/deaths_test.csv"), append = T, row.names = F)
 
 # write.table(cases, "data/cases.csv", sep = ",", col.names = !file.exists("data/cases.csv"), append = T, row.names = F)
 # write.table(cases, "data/deaths.csv", sep = ",", col.names = !file.exists("data/deaths.csv"), append = T, row.names = F)
